a63154478df3489473cd44b19601cc66bbc36826
Dominik Rosiek <drosiek@sumologic.com>
2021-05-31 10:11:22 +0200

    chore(vagrant): add vagrant environment
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+unless Vagrant.has_plugin?("vagrant-disksize")
+  puts "vagrant-disksize plugin unavailable\n" +
+       "please install it via 'vagrant plugin install vagrant-disksize'"
+  exit 1
+end
+
+Vagrant.configure('2') do |config|
+  config.vm.box = 'ubuntu/focal64'
+  config.disksize.size = '50GB'
+  config.vm.box_check_update = false
+  config.vm.host_name = 'sumologic-otel-collector'
+  config.vm.network :private_network, ip: "192.168.79.13"
+
+  config.vm.provider 'virtualbox' do |vb|
+    vb.gui = false
+    vb.cpus = 8
+    vb.memory = 16384
+    vb.name = 'sumologic-otel-collector'
+  end
+
+  config.vm.provision 'shell', path: 'vagrant/provision.sh'
+
+  config.vm.synced_folder ".", "/sumologic"
+end
---
+++ vagrant/provision.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export BUILDER_VERSION=0.27.0
+export GO_VERSION=1.16.4
+
+# Install opentelemetry-collector-builder
+curl -LJ \
+    "https://github.com/open-telemetry/opentelemetry-collector-builder/releases/download/v${BUILDER_VERSION}/opentelemetry-collector-builder_${BUILDER_VERSION}_linux_amd64" \
+    -o /usr/local/bin/opentelemetry-collector-builder \
+    && chmod +x /usr/local/bin/opentelemetry-collector-builder
+
+sudo apt update -y
+sudo apt install -y \
+    make \
+    gcc
+
+# Install Go
+curl -LJ "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.linux-amd64.tar.gz \
+    && rm -rf /usr/local/go \
+    && tar -C /usr/local -xzf go.linux-amd64.tar.gz \
+    && rm go.linux-amd64.tar.gz \
+    && ln -s /usr/local/go/bin/go /usr/local/bin
