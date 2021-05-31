fab805eb9a84543e5b7870317471cabaf29005fc
Dominik Rosiek <drosiek@sumologic.com>
2021-05-31 11:11:37 +0200

    feat(processors): add sumologicsyslogprocessor
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

---
+++ pkg/processor/sumologicsyslogprocessor/processor_test.go
@@ -0,0 +1,67 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sumologicsyslogprocessor
+
+import (
+	"context"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/consumer/pdata"
+)
+
+func TestProcessLogs(t *testing.T) {
+	lines := []string{
+		`<13> Example log`,
+		`<334> Another example log`,
+		`Plain text`,
+	}
+
+	facilities := []string{
+		`user-level messages`,
+		`syslog`,
+		`syslog`,
+	}
+
+	logs := pdata.NewLogs()
+	logs.ResourceLogs().Resize(1)
+	logs.ResourceLogs().At(0).InstrumentationLibraryLogs().Resize(len(lines))
+	lgs := logs.ResourceLogs().At(0).InstrumentationLibraryLogs().At(0).Logs()
+
+	for _, line := range lines {
+		lr := pdata.NewLogRecord()
+		lr.Body().SetStringVal(line)
+		lgs.Append(lr)
+	}
+	lgs.At(1).Attributes().InsertString("facility_name", "pre filled facility")
+
+	ctx := context.Background()
+	processor := &sumologicSyslogProcessor{
+		syslogFacilityAttrName: "facility_name",
+		syslogFacilityRegex:    regexp.MustCompile(`^<(?P<number>\d+)>`),
+	}
+
+	result, err := processor.ProcessLogs(ctx, logs)
+	require.NoError(t, err)
+
+	for i, line := range facilities {
+		attrs := result.ResourceLogs().At(0).InstrumentationLibraryLogs().At(0).Logs().At(i).Attributes()
+		attr, ok := attrs.Get("facility_name")
+		require.True(t, ok)
+		assert.Equal(t, line, attr.StringVal())
+	}
+}
