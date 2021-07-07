55a2b19a507676540689ed7087134937c4c92fea
Patryk Małek <pmalek@sumologic.com>
2021-07-07 11:15:53 +0200

    fix(telegrafreceiver): use correct context when consuming metrics (#86)

--- pkg/receiver/telegrafreceiver/receiver.go
+++ pkg/receiver/telegrafreceiver/receiver.go
@@ -64,7 +64,7 @@ func (r *telegrafreceiver) Start(ctx context.Context, host component.Host) error
 		ch := make(chan telegraf.Metric)
 		go func() {
 			if rErr := r.agent.RunWithChannel(rctx, ch); rErr != nil {
-				r.logger.Error("Problem starting receiver: %v", zap.Error(rErr))
+				r.logger.Error("Problem starting receiver", zap.Error(rErr))
 			}
 		}()
 
@@ -96,7 +96,7 @@ func (r *telegrafreceiver) Start(ctx context.Context, host component.Host) error
 						continue
 					}
 
-					if fErr = r.consumer.ConsumeMetrics(ctx, ms); fErr != nil {
+					if fErr = r.consumer.ConsumeMetrics(rctx, ms); fErr != nil {
 						r.logger.Error("ConsumeMetrics() error",
 							zap.String("error", fErr.Error()),
 						)
