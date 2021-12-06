c160e718a0236e3413df0d0710c3c0f80c554a50
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-12-06 11:09:51 +0100

    chore: add LuaJIT to vagrant environment

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -78,3 +78,13 @@ add-apt-repository \
    stable"
 apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
+
+# Install LuaJIT
+wget https://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
+tar -xzf LuaJIT-2.1.0-beta3.tar.gz
+cd LuaJIT-2.1.0-beta3/
+make && make install
+cd src
+ln -sf luajit /usr/local/bin/luajit
+cp libluajit.so /usr/local/lib/libluajit.so
+ldconfig
