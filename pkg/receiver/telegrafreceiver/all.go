c50767d9cc144ea5172661e47552e13f7895c7b0
Patryk Małek <pmalek@sumologic.com>
2022-03-25 16:35:58 +0100

    chore(deps): bump telegraf to 1.22.0-sumo-0 (#494)

--- pkg/receiver/telegrafreceiver/all.go
+++ pkg/receiver/telegrafreceiver/all.go
@@ -15,8 +15,5 @@
 package telegrafreceiver
 
 import (
-	// _ "github.com/influxdata/telegraf/plugins/aggregators/all"
 	_ "github.com/influxdata/telegraf/plugins/inputs/all"
-	// _ "github.com/influxdata/telegraf/plugins/outputs/all"
-	// _ "github.com/influxdata/telegraf/plugins/processors/all"
 )
