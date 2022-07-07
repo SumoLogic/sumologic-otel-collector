SumoLogic/sumologic-otel-collector

commit ac4fcca943760849f032b6940e78be2ee72c6749

fix(sumologicexporter): treat resource attributes as fields for otlp (#536)
* fix(sumologicexporter): treat resource attributes as fields for otlp

* Update pkg/exporter/sumologicexporter/exporter_test.go

Co-authored-by: Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>

Co-authored-by: Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>

pkg/exporter/sumologicexporter/exporter.go
@@ -224,13 +224,6 @@ func newTracesExporter(
 // It returns the number of unsent logs and an error which contains a list of dropped records
 // so they can be handled by OTC retry mechanism
 func (se *sumologicexporter) pushLogsData(ctx context.Context, ld pdata.Logs) error {
-	var (
-		currentMetadata  fields = newFields(pdata.NewAttributeMap())
-		previousMetadata fields = newFields(pdata.NewAttributeMap())
-		errs             []error
-		droppedRecords   []logPair
-	)
-
 	compr, err := se.getCompressor()
 	if err != nil {
 		return consumererror.NewLogs(err, ld)
@@ -250,6 +243,22 @@ func (se *sumologicexporter) pushLogsData(ctx context.Context, ld pdata.Logs) er
 		metricsUrl,
 		logsUrl,
 		tracesUrl,
+	)
+
+	// Follow different execution path for OTLP format
+	if sdr.config.LogFormat == OTLPLogFormat {
+		if err := sdr.sendOTLPLogs(ctx, ld); err != nil {
+			se.handleUnauthorizedErrors(ctx, err)
+			return consumererror.NewLogs(err, ld)
+		}
+		return nil
+	}
+
+	var (
+		currentMetadata  fields = newFields(pdata.NewAttributeMap())
+		previousMetadata fields = newFields(pdata.NewAttributeMap())
+		errs             []error
+		droppedRecords   []logPair
 	)
 
 	// Iterate over ResourceLogs
@@ -298,7 +307,7 @@ func (se *sumologicexporter) pushLogsData(ctx context.Context, ld pdata.Logs) er
 				// If metadata differs from currently buffered, flush the buffer
 				if !currentMetadata.equals(previousMetadata) && !previousMetadata.isEmpty() {
 					var dropped []logPair
-					dropped, err = sdr.sendLogs(ctx, previousMetadata)
+					dropped, err = sdr.sendNonOTLPLogs(ctx, previousMetadata)
 					if err != nil {
 						errs = append(errs, err)
 						droppedRecords = append(droppedRecords, dropped...)
@@ -321,7 +330,7 @@ func (se *sumologicexporter) pushLogsData(ctx context.Context, ld pdata.Logs) er
 	}
 
 	// Flush pending logs
-	dropped, err := sdr.sendLogs(ctx, previousMetadata)
+	dropped, err := sdr.sendNonOTLPLogs(ctx, previousMetadata)
 	if err != nil {
 		droppedRecords = append(droppedRecords, dropped...)
 		errs = append(errs, err)
@@ -351,14 +360,6 @@ func (se *sumologicexporter) pushLogsData(ctx context.Context, ld pdata.Logs) er
 // it returns number of unsent metrics and error which contains list of dropped records
 // so they can be handle by the OTC retry mechanism
 func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pdata.Metrics) error {
-	var (
-		currentMetadata  fields = newFields(pdata.NewAttributeMap())
-		previousMetadata fields = newFields(pdata.NewAttributeMap())
-		errs             []error
-		droppedRecords   []metricPair
-		attributes       pdata.AttributeMap
-	)
-
 	compr, err := se.getCompressor()
 	if err != nil {
 		return consumererror.NewMetrics(err, md)
@@ -380,12 +381,28 @@ func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pdata.Metri
 		tracesUrl,
 	)
 
+	// Follow different execution path for OTLP format
+	if sdr.config.MetricFormat == OTLPMetricFormat {
+		if err := sdr.sendOTLPMetrics(ctx, md); err != nil {
+			se.handleUnauthorizedErrors(ctx, err)
+			return consumererror.NewMetrics(err, md)
+		}
+		return nil
+	}
+
+	var (
+		currentMetadata  fields = newFields(pdata.NewAttributeMap())
+		previousMetadata fields = newFields(pdata.NewAttributeMap())
+		errs             []error
+		droppedRecords   []metricPair
+	)
+
 	// Iterate over ResourceMetrics
 	rms := md.ResourceMetrics()
 	for i := 0; i < rms.Len(); i++ {
 		rm := rms.At(i)
 
-		attributes = rm.Resource().Attributes()
+		attributes := rm.Resource().Attributes()
 
 		// drop routing attribute
 		se.dropRoutingAttribute(attributes)
@@ -420,7 +437,7 @@ func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pdata.Metri
 				if !currentMetadata.equals(previousMetadata) && !previousMetadata.isEmpty() {
 					var dropped []metricPair
 					var err error
-					dropped, err = sdr.sendMetrics(ctx, previousMetadata)
+					dropped, err = sdr.sendNonOTLPMetrics(ctx, previousMetadata)
 					if err != nil {
 						errs = append(errs, err)
 						droppedRecords = append(droppedRecords, dropped...)
@@ -443,7 +460,7 @@ func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pdata.Metri
 	}
 
 	// Flush pending metrics
-	dropped, err := sdr.sendMetrics(ctx, previousMetadata)
+	dropped, err := sdr.sendNonOTLPMetrics(ctx, previousMetadata)
 	if err != nil {
 		droppedRecords = append(droppedRecords, dropped...)
 		errs = append(errs, err)
@@ -489,8 +506,6 @@ func (se *sumologicexporter) handleUnauthorizedErrors(ctx context.Context, errs 
 }
 
 func (se *sumologicexporter) pushTracesData(ctx context.Context, td pdata.Traces) error {
-	var currentMetadata fields = newFields(pdata.NewAttributeMap())
-
 	compr, err := se.getCompressor()
 	if err != nil {
 		return consumererror.NewTraces(err, td)
@@ -518,13 +533,9 @@ func (se *sumologicexporter) pushTracesData(ctx context.Context, td pdata.Traces
 		se.dropRoutingAttribute(rss.At(i).Resource().Attributes())
 	}
 
-	err = sdr.sendTraces(ctx, td, currentMetadata)
+	err = sdr.sendTraces(ctx, td)
 	se.handleUnauthorizedErrors(ctx, err)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func (se *sumologicexporter) getCompressor() (compressor, error) {

pkg/exporter/sumologicexporter/sender.go
@@ -25,11 +25,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/SumoLogic/sumologic-otel-collector/pkg/exporter/sumologicexporter/internal/observability"
 	"go.opentelemetry.io/collector/model/otlp"
 	"go.opentelemetry.io/collector/model/pdata"
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
+
+	"github.com/SumoLogic/sumologic-otel-collector/pkg/exporter/sumologicexporter/internal/observability"
 )
 
 var (
@@ -414,13 +415,12 @@ func isEmptyAttributeValue(att pdata.AttributeValue) bool {
 		t == pdata.AttributeValueTypeBytes && len(att.BytesVal()) > 0)
 }
 
-// sendLogs sends log records from the logBuffer formatted according
+// sendNonOTLPLogs sends log records from the logBuffer formatted according
 // to configured LogFormat and as the result of execution
 // returns array of records which has not been sent correctly and error
-func (s *sender) sendLogs(ctx context.Context, flds fields) ([]logPair, error) {
-	// Follow different execution path for OTLP format
+func (s *sender) sendNonOTLPLogs(ctx context.Context, flds fields) ([]logPair, error) {
 	if s.config.LogFormat == OTLPLogFormat {
-		return s.sendOTLPLogs(ctx, flds)
+		return nil, fmt.Errorf("Attempting to send OTLP logs as non-OTLP data")
 	}
 
 	var (
@@ -485,52 +485,43 @@ func (s *sender) sendLogs(ctx context.Context, flds fields) ([]logPair, error) {
 	return droppedRecords, nil
 }
 
-// sendLogs sends log records from the logBuffer in OTLP format and as a result
-// it returns an array of records which has not been sent correctly and an error.
 // TODO: add support for HTTP limits
-func (s *sender) sendOTLPLogs(ctx context.Context, flds fields) ([]logPair, error) {
-	ld := pdata.NewLogs()
-	rl := ld.ResourceLogs().AppendEmpty()
-	ill := rl.InstrumentationLibraryLogs().AppendEmpty()
-	logs := ill.LogRecords()
-	capacity := len(s.logBuffer)
-	logs.EnsureCapacity(capacity)
-	for _, record := range s.logBuffer {
-		log := logs.AppendEmpty()
-		record.log.CopyTo(log)
-		log.Attributes().Clear()
-		log.Attributes().EnsureCapacity(record.attributes.Len())
+func (s *sender) sendOTLPLogs(ctx context.Context, ld pdata.Logs) error {
+	rls := ld.ResourceLogs()
+	for i := 0; i < rls.Len(); i++ {
+		rl := rls.At(i)
 
 		if s.config.TranslateAttributes {
-			translateAttributes(record.attributes).CopyTo(log.Attributes())
-		} else {
-			record.attributes.CopyTo(log.Attributes())
+			translateAttributes(rl.Resource().Attributes()).
+				CopyTo(rl.Resource().Attributes())
 		}
 
-		// Clear timestamp if required
+		// Clear timestamps if required
 		if s.config.ClearLogsTimestamp {
-			log.SetTimestamp(0)
+			slgs := rl.ScopeLogs()
+			for j := 0; j < slgs.Len(); j++ {
+				log := slgs.At(j)
+				for k := 0; k < log.LogRecords().Len(); k++ {
+					log.LogRecords().At(k).SetTimestamp(0)
+				}
+			}
 		}
-	}
 
-	s.addResourceAttributes(rl.Resource().Attributes(), flds)
+		s.addSourceResourceAttributes(rl.Resource().Attributes())
+	}
 
 	body, err := logsMarshaler.MarshalLogs(ld)
 	if err != nil {
-		return s.logBuffer, err
+		return err
 	}
 
-	if err := s.send(ctx, LogsPipeline, newCountingReader(capacity).withBytes(body), flds); err != nil {
-		return s.logBuffer, err
-	}
-	return nil, nil
+	return s.send(ctx, LogsPipeline, newCountingReader(ld.LogRecordCount()).withBytes(body), fields{})
 }
 
-// sendMetrics sends metrics in right format basing on the s.config.MetricFormat
-func (s *sender) sendMetrics(ctx context.Context, flds fields) ([]metricPair, error) {
-	// Follow different execution path for OTLP format
+// sendNonOTLPMetrics sends metrics in right format basing on the s.config.MetricFormat
+func (s *sender) sendNonOTLPMetrics(ctx context.Context, flds fields) ([]metricPair, error) {
 	if s.config.MetricFormat == OTLPMetricFormat {
-		return s.sendOTLPMetrics(ctx, flds)
+		return nil, fmt.Errorf("Attempting to send OTLP metrics as non-OTLP data")
 	}
 
 	var (
@@ -597,32 +588,25 @@ func (s *sender) sendMetrics(ctx context.Context, flds fields) ([]metricPair, er
 	return droppedRecords, nil
 }
 
-// sendMetrics sends metric records from the metricBuffer in OTLP format and as a result
-// it returns an array of records which has not been sent correctly and an error.
-// TODO: add support for HTTP limits
-func (s *sender) sendOTLPMetrics(ctx context.Context, flds fields) ([]metricPair, error) {
-	md := pdata.NewMetrics()
+func (s *sender) sendOTLPMetrics(ctx context.Context, md pdata.Metrics) error {
 	rms := md.ResourceMetrics()
-	capacity := len(s.metricBuffer)
-	rms.EnsureCapacity(capacity)
-	for _, record := range s.metricBuffer {
-		rm := rms.AppendEmpty()
-		record.attributes.CopyTo(rm.Resource().Attributes())
-		s.addResourceAttributes(rm.Resource().Attributes(), flds)
-		ilm := rm.InstrumentationLibraryMetrics().AppendEmpty()
-		ms := ilm.Metrics().AppendEmpty()
-		record.metric.CopyTo(ms)
+	for i := 0; i < rms.Len(); i++ {
+		rm := rms.At(i)
+
+		if s.config.TranslateAttributes {
+			translateAttributes(rm.Resource().Attributes()).
+				CopyTo(rm.Resource().Attributes())
+		}
+
+		s.addSourceResourceAttributes(rm.Resource().Attributes())
 	}
 
 	body, err := metricsMarshaler.MarshalMetrics(md)
 	if err != nil {
-		return s.metricBuffer, err
+		return err
 	}
 
-	if err := s.send(ctx, MetricsPipeline, newCountingReader(capacity).withBytes(body), flds); err != nil {
-		return s.metricBuffer, err
-	}
-	return nil, nil
+	return s.send(ctx, MetricsPipeline, newCountingReader(md.DataPointCount()).withBytes(body), fields{})
 }
 
 // appendAndSend appends line to the request body that will be sent and sends
@@ -669,25 +653,25 @@ func (s *sender) appendAndSend(
 }
 
 // sendTraces sends traces in right format basing on the s.config.TraceFormat
-func (s *sender) sendTraces(ctx context.Context, td pdata.Traces, flds fields) error {
+func (s *sender) sendTraces(ctx context.Context, td pdata.Traces) error {
 	if s.config.TraceFormat == OTLPTraceFormat {
-		return s.sendOTLPTraces(ctx, td, flds)
+		return s.sendOTLPTraces(ctx, td)
 	}
 	return nil
 }
 
 // sendOTLPTraces sends trace records in OTLP format
-func (s *sender) sendOTLPTraces(ctx context.Context, td pdata.Traces, flds fields) error {
+func (s *sender) sendOTLPTraces(ctx context.Context, td pdata.Traces) error {
 	capacity := td.SpanCount()
 	for i := 0; i < td.ResourceSpans().Len(); i++ {
-		s.addResourceAttributes(td.ResourceSpans().At(i).Resource().Attributes(), flds)
+		s.addSourceResourceAttributes(td.ResourceSpans().At(i).Resource().Attributes())
 	}
 
 	body, err := tracesMarshaler.MarshalTraces(td)
 	if err != nil {
 		return err
 	}
-	if err := s.send(ctx, TracesPipeline, newCountingReader(capacity).withBytes(body), flds); err != nil {
+	if err := s.send(ctx, TracesPipeline, newCountingReader(capacity).withBytes(body), fields{}); err != nil {
 		return err
 	}
 	return nil
@@ -704,7 +688,7 @@ func (s *sender) batchLog(ctx context.Context, log logPair, metadata fields) ([]
 	s.logBuffer = append(s.logBuffer, log)
 
 	if s.countLogs() >= maxBufferSize {
-		dropped, err := s.sendLogs(ctx, metadata)
+		dropped, err := s.sendNonOTLPLogs(ctx, metadata)
 		s.cleanLogsBuffer()
 		return dropped, err
 	}
@@ -728,7 +712,7 @@ func (s *sender) batchMetric(ctx context.Context, metric metricPair, metadata fi
 	s.metricBuffer = append(s.metricBuffer, metric)
 
 	if s.countMetrics() >= maxBufferSize {
-		dropped, err := s.sendMetrics(ctx, metadata)
+		dropped, err := s.sendNonOTLPMetrics(ctx, metadata)
 		s.cleanMetricBuffer()
 		return dropped, err
 	}
@@ -756,6 +740,10 @@ func addCompressHeader(req *http.Request, enc CompressEncodingType) error {
 }
 
 func addSourcesHeaders(req *http.Request, sources sourceFormats, flds fields) {
+	if !flds.isInitialized() {
+		return
+	}
+
 	if sources.host.isSet() {
 		req.Header.Add(headerHost, sources.host.format(flds))
 	}
@@ -833,7 +821,13 @@ func (s *sender) addRequestHeaders(req *http.Request, pipeline PipelineType, fld
 	return nil
 }
 
-func (s *sender) addResourceAttributes(attrs pdata.AttributeMap, flds fields) {
+// addSourceResourceAttributes adds source related attributes:
+// * source category
+// * source host
+// * source name
+// to the provided attribute map using the provided fields as values source and using
+// the source templates for formatting.
+func (s *sender) addSourceRelatedResourceAttributesFromFields(attrs pdata.AttributeMap, flds fields) {
 	if s.sources.host.isSet() {
 		attrs.InsertString(attributeKeySourceHost, s.sources.host.format(flds))
 	}
@@ -842,6 +836,33 @@ func (s *sender) addResourceAttributes(attrs pdata.AttributeMap, flds fields) {
 	}
 	if s.sources.category.isSet() {
 		attrs.InsertString(attributeKeySourceCategory, s.sources.category.format(flds))
+	}
+}
+
+// addSourceResourceAttributes adds source related attributes:
+// * source category
+// * source host
+// * source name
+// to the provided attribute map, according to the corresponding templates.
+//
+// When those attributes are already in the attribute map then nothing is
+// changed since attributes that are provided with data have precedence over
+// exporter configuration.
+func (s *sender) addSourceResourceAttributes(attrs pdata.AttributeMap) {
+	if s.sources.host.isSet() {
+		if _, ok := attrs.Get(attributeKeySourceHost); !ok {
+			attrs.InsertString(attributeKeySourceHost, s.sources.host.formatPdataMap(attrs))
+		}
+	}
+	if s.sources.name.isSet() {
+		if _, ok := attrs.Get(attributeKeySourceName); !ok {
+			attrs.InsertString(attributeKeySourceName, s.sources.name.formatPdataMap(attrs))
+		}
+	}
+	if s.sources.category.isSet() {
+		if _, ok := attrs.Get(attributeKeySourceCategory); !ok {
+			attrs.InsertString(attributeKeySourceCategory, s.sources.category.formatPdataMap(attrs))
+		}
 	}
 }
 
@@ -869,5 +890,4 @@ func (s *sender) recordMetrics(duration time.Duration, count int64, req *http.Re
 	if err := observability.RecordRequestsSent(statusCode, req.URL.String(), string(pipeline), id); err != nil {
 		s.logger.Debug("error for recording metric for sent request", zap.Error(err))
 	}
-
 }

pkg/exporter/sumologicexporter/exporter_test.go
@@ -276,6 +276,8 @@ func TestPushInvalidCompressor(t *testing.T) {
 }
 
 func TestPushFailedBatch(t *testing.T) {
+	t.Parallel()
+
 	test := prepareExporterTest(t, createTestConfig(), []func(w http.ResponseWriter, req *http.Request){
 		func(w http.ResponseWriter, req *http.Request) {
 			w.WriteHeader(500)
@@ -392,9 +394,12 @@ func TestPushOTLPLogs_AttributeTranslation(t *testing.T) {
 			name: "enabled",
 			configFunc: func() *Config {
 				config := createTestConfig()
-				config.MetadataAttributes = []string{`host\.name`}
-				config.SourceCategory = "%{host.name}"
-				config.SourceHost = "%{host}"
+				// NOTE: MetadataAttributes does not have an impact on exporter
+				// behavior when using OTLP. What gets sent as fields is purely
+				// depdendent on what's in resource attributes.
+				config.MetadataAttributes = []string{}
+				config.SourceCategory = "category_with_host_template_%{host.name}"
+				config.SourceHost = "%{host.name}"
 				config.LogFormat = OTLPLogFormat
 				config.TranslateAttributes = true
 				return config
@@ -402,18 +407,22 @@ func TestPushOTLPLogs_AttributeTranslation(t *testing.T) {
 			callbacks: []func(w http.ResponseWriter, req *http.Request){
 				func(w http.ResponseWriter, req *http.Request) {
 					body := extractBody(t, req)
-					//nolint:lll
-					assert.Equal(t, "\n\xae\x01\nB\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n!\n\x0f_sourceCategory\x12\x0e\n\fharry-potter\x12h\n\x00\x12d*\r\n\vExample log2\x16\n\x04host\x12\x0e\n\fharry-potter2\x18\n\fInstanceType\x12\b\n\x06wizard2\x1d\n\v_sourceName\x12\x0e\n\f/tmp/log.logJ\x00R\x00", body)
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "host=harry-potter", req.Header.Get("X-Sumo-Fields"), "X-Sumo-Fields")
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "harry-potter", req.Header.Get("X-Sumo-Category"), "X-Sumo-Category")
 
-					// This gets the value from 'host.name' because we do not disallow
-					// using Sumo schema and 'host.name' from OT convention
-					// translates into 'host' in Sumo convention
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "harry-potter", req.Header.Get("X-Sumo-Host"), "X-Sumo-Host")
+					//nolint:lll
+					assert.Equal(t, "\n\xcb\x01\n\xaf\x01\n\x16\n\x04host\x12\x0e\n\fharry-potter\n\x18\n\fInstanceType\x12\b\n\x06wizard\n\x1d\n\v_sourceName\x12\x0e\n\f/tmp/log.log\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n=\n\x0f_sourceCategory\x12*\n(category_with_host_template_harry-potter\x12\x17\n\x00\x12\x13*\r\n\vExample logJ\x00R\x00", body)
+
+					assert.Empty(t, req.Header.Get("X-Sumo-Fields"),
+						"We should not get X-Sumo-Fields header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Category"),
+						"We should not get X-Sumo-Category header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Name"),
+						"We should not get X-Sumo-Name header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Host"),
+						"We should not get X-Sumo-Host header when sending data with OTLP",
+					)
 				},
 			},
 		},
@@ -421,9 +430,12 @@ func TestPushOTLPLogs_AttributeTranslation(t *testing.T) {
 			name: "disabled",
 			configFunc: func() *Config {
 				config := createTestConfig()
-				config.MetadataAttributes = []string{`host\.name`}
-				config.SourceCategory = "%{host.name}"
-				config.SourceHost = "%{host}"
+				// NOTE: MetadataAttributes does not have an impact on exporter
+				// behavior when using OTLP. What gets sent as fields is purely
+				// depdendent on what's in resource attributes.
+				config.MetadataAttributes = []string{}
+				config.SourceCategory = "category_with_host_template_%{host.name}"
+				config.SourceHost = "%{host.name}"
 				config.LogFormat = OTLPLogFormat
 				config.TranslateAttributes = false
 				return config
@@ -431,15 +443,22 @@ func TestPushOTLPLogs_AttributeTranslation(t *testing.T) {
 			callbacks: []func(w http.ResponseWriter, req *http.Request){
 				func(w http.ResponseWriter, req *http.Request) {
 					body := extractBody(t, req)
-					//nolint:lll
-					assert.Equal(t, "\n\xb4\x01\n?\n\x1a\n\v_sourceHost\x12\v\n\tundefined\n!\n\x0f_sourceCategory\x12\x0e\n\fharry-potter\x12q\n\x00\x12m*\r\n\vExample log2\x1b\n\thost.name\x12\x0e\n\fharry-potter2\x15\n\thost.type\x12\b\n\x06wizard2$\n\x12file.path.resolved\x12\x0e\n\f/tmp/log.logJ\x00R\x00", body)
 
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "host.name=harry-potter", req.Header.Get("X-Sumo-Fields"), "X-Sumo-Fields")
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "harry-potter", req.Header.Get("X-Sumo-Category"), "X-Sumo-Category")
-					// TODO: Revisit: this should probably be empty when sending with OTLP
-					assert.Equal(t, "undefined", req.Header.Get("X-Sumo-Host"), "X-Sumo-Host")
+					//nolint:lll
+					assert.Equal(t, "\n\xd4\x01\n\xb8\x01\n\x1b\n\thost.name\x12\x0e\n\fharry-potter\n\x15\n\thost.type\x12\b\n\x06wizard\n$\n\x12file.path.resolved\x12\x0e\n\f/tmp/log.log\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n=\n\x0f_sourceCategory\x12*\n(category_with_host_template_harry-potter\x12\x17\n\x00\x12\x13*\r\n\vExample logJ\x00R\x00", body)
+
+					assert.Empty(t, req.Header.Get("X-Sumo-Fields"),
+						"We should not get X-Sumo-Fields header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Category"),
+						"We should not get X-Sumo-Category header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Name"),
+						"We should not get X-Sumo-Name header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Host"),
+						"We should not get X-Sumo-Host header when sending data with OTLP",
+					)
 				},
 			},
 		},
@@ -1425,51 +1444,42 @@ func TestPushOTLPMetrics_AttributeTranslation(t *testing.T) {
 	}
 
 	testcases := []struct {
-		name            string
-		cfgFn           func() *Config
-		expectedHeaders map[string]string
-		expectedBody    string
+		name         string
+		cfgFn        func() *Config
+		expectedBody string
 	}{
 		{
 			name: "enabled",
 			cfgFn: func() *Config {
 				cfg := createConfig()
-				cfg.MetadataAttributes = []string{`host\.name`}
-				cfg.SourceCategory = "%{host.name}"
+				// NOTE: MetadataAttributes does not have an impact on exporter
+				// behavior when using OTLP. What gets sent as fields is purely
+				// dependent on what's in resource attributes.
+				cfg.MetadataAttributes = []string{}
+				cfg.SourceCategory = "source_category_with_hostname_%{host.name}"
 				cfg.SourceHost = "%{host}"
 				// This is and should be done by default:
 				// cfg.TranslateAttributes = true
 				return cfg
 			},
-			expectedHeaders: map[string]string{
-				"Content-Type":    "application/x-protobuf",
-				"X-Sumo-Category": "harry-potter",
-
-				// This gets the value from 'host.name' because we do not disallow
-				// using Sumo schema and 'host.name' from OT convention
-				// translates into 'host' in Sumo convention
-				"X-Sumo-Host": "harry-potter",
-			},
 			//nolint:lll
-			expectedBody: "\n\xdb\x01\n\xa3\x01\n\x14\n\x04test\x12\f\n\ntest_value\n\x17\n\x05test2\x12\x0e\n\fsecond_value\n\x16\n\x04host\x12\x0e\n\fharry-potter\n\x18\n\fInstanceType\x12\b\n\x06wizard\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n!\n\x0f_sourceCategory\x12\x0e\n\fharry-potter\x123\n\x00\x12/\n\x10test.metric.data\x1a\x05bytes:\x14\n\x12\x19\x00\x12\x94\v\xd1\x00H\x161\xa48\x00\x00\x00\x00\x00\x00",
+			expectedBody: "\n\xf9\x01\n\xc1\x01\n\x14\n\x04test\x12\f\n\ntest_value\n\x17\n\x05test2\x12\x0e\n\fsecond_value\n\x16\n\x04host\x12\x0e\n\fharry-potter\n\x18\n\fInstanceType\x12\b\n\x06wizard\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n?\n\x0f_sourceCategory\x12,\n*source_category_with_hostname_harry-potter\x123\n\x00\x12/\n\x10test.metric.data\x1a\x05bytes:\x14\n\x12\x19\x00\x12\x94\v\xd1\x00H\x161\xa48\x00\x00\x00\x00\x00\x00",
 		},
 		{
 			name: "disabled",
 			cfgFn: func() *Config {
 				cfg := createConfig()
-				cfg.MetadataAttributes = []string{`host\.name`}
-				cfg.SourceCategory = "%{host.name}"
-				cfg.SourceHost = "%{host}"
+				// NOTE: MetadataAttributes does not have an impact on exporter
+				// behavior when using OTLP. What gets sent as fields is purely
+				// depdendent on what's in resource attributes.
+				cfg.MetadataAttributes = []string{}
+				cfg.SourceCategory = "source_category_with_hostname_%{host.name}"
+				cfg.SourceHost = "%{host.name}"
 				cfg.TranslateAttributes = false
 				return cfg
 			},
-			expectedHeaders: map[string]string{
-				"Content-Type":    "application/x-protobuf",
-				"X-Sumo-Category": "harry-potter",
-				"X-Sumo-Host":     "undefined",
-			},
 			//nolint:lll
-			expectedBody: "\n\xda\x01\n\xa2\x01\n\x14\n\x04test\x12\f\n\ntest_value\n\x17\n\x05test2\x12\x0e\n\fsecond_value\n\x1b\n\thost.name\x12\x0e\n\fharry-potter\n\x15\n\thost.type\x12\b\n\x06wizard\n\x1a\n\v_sourceHost\x12\v\n\tundefined\n!\n\x0f_sourceCategory\x12\x0e\n\fharry-potter\x123\n\x00\x12/\n\x10test.metric.data\x1a\x05bytes:\x14\n\x12\x19\x00\x12\x94\v\xd1\x00H\x161\xa48\x00\x00\x00\x00\x00\x00",
+			expectedBody: "\n\xfb\x01\n\xc3\x01\n\x14\n\x04test\x12\f\n\ntest_value\n\x17\n\x05test2\x12\x0e\n\fsecond_value\n\x1b\n\thost.name\x12\x0e\n\fharry-potter\n\x15\n\thost.type\x12\b\n\x06wizard\n\x1d\n\v_sourceHost\x12\x0e\n\fharry-potter\n?\n\x0f_sourceCategory\x12,\n*source_category_with_hostname_harry-potter\x123\n\x00\x12/\n\x10test.metric.data\x1a\x05bytes:\x14\n\x12\x19\x00\x12\x94\v\xd1\x00H\x161\xa48\x00\x00\x00\x00\x00\x00",
 		},
 	}
 
@@ -1487,11 +1497,21 @@ func TestPushOTLPMetrics_AttributeTranslation(t *testing.T) {
 			callbacks := []func(w http.ResponseWriter, req *http.Request){
 				func(w http.ResponseWriter, req *http.Request) {
 					assert.Equal(t, tc.expectedBody, extractBody(t, req))
-					for header, expectedValue := range tc.expectedHeaders {
-						assert.Equalf(t, expectedValue, req.Header.Get(header),
-							"Unexpected value in header: %s", header,
-						)
-					}
+
+					assert.Equal(t, "application/x-protobuf", req.Header.Get("Content-Type"))
+
+					assert.Empty(t, req.Header.Get("X-Sumo-Fields"),
+						"We should not get X-Sumo-Fields header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Category"),
+						"We should not get X-Sumo-Category header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Name"),
+						"We should not get X-Sumo-Name header when sending data with OTLP",
+					)
+					assert.Empty(t, req.Header.Get("X-Sumo-Host"),
+						"We should not get X-Sumo-Host header when sending data with OTLP",
+					)
 				},
 			}
 			test := prepareExporterTest(t, config, callbacks)

pkg/exporter/sumologicexporter/fields.go
@@ -24,13 +24,19 @@ import (
 
 // fields represents metadata
 type fields struct {
-	orig pdata.AttributeMap
+	orig        pdata.AttributeMap
+	initialized bool
 }
 
 func newFields(attrMap pdata.AttributeMap) fields {
 	return fields{
-		orig: attrMap,
+		orig:        attrMap,
+		initialized: true,
 	}
+}
+
+func (f fields) isInitialized() bool {
+	return f.initialized
 }
 
 func (f fields) isEmpty() bool {
@@ -47,6 +53,10 @@ func (f fields) equals(other fields) bool {
 
 // string returns fields as ordered key=value string with `, ` as separator
 func (f fields) string() string {
+	if !f.initialized {
+		return ""
+	}
+
 	returnValue := make([]string, 0, f.orig.Len())
 
 	f.orig.Range(func(k string, v pdata.AttributeValue) bool {

commit 1ff2e9ee997678c9022c7ac5ce280ddae9020aad

fix(sumologicexporter): translate Telegraf metrics with OTLP format
This change fixes a regression introduced in https://github.com/SumoLogic/sumologic-otel-collector/pull/536
that caused the metric name translations
(the feature under the `translate_telegraf_attributes` setting)
to not work when using the OTLP format.
This change refactors the code so that all modifications to metrics
(attribute translation, metrics translation, dropping routing attribute)
are done before branching the code into different sending formats.

pkg/exporter/sumologicexporter/exporter.go
@@ -283,19 +283,6 @@ func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pmetric.Met
 		tracesUrl,
 	)
 
-	// Follow different execution path for OTLP format
-	if sdr.config.MetricFormat == OTLPMetricFormat {
-		if err := sdr.sendOTLPMetrics(ctx, md); err != nil {
-			se.handleUnauthorizedErrors(ctx, err)
-			return consumererror.NewMetrics(err, md)
-		}
-		return nil
-	}
-
-	var (
-		errs []error
-	)
-
 	// Transform metrics metadata
 	// this includes dropping the routing attribute and translating attributes
 	rms := md.ResourceMetrics()
@@ -321,7 +308,16 @@ func (se *sumologicexporter) pushMetricsData(ctx context.Context, md pmetric.Met
 		}
 	}
 
-	droppedMetrics, errs := sdr.sendNonOTLPMetrics(ctx, md)
+	var droppedMetrics pmetric.Metrics
+	var errs []error
+	if sdr.config.MetricFormat == OTLPMetricFormat {
+		if err := sdr.sendOTLPMetrics(ctx, md); err != nil {
+			droppedMetrics = md
+			errs = []error{err}
+		}
+	} else {
+		droppedMetrics, errs = sdr.sendNonOTLPMetrics(ctx, md)
+	}
 
 	if len(errs) > 0 {
 		se.handleUnauthorizedErrors(ctx, errs...)

pkg/exporter/sumologicexporter/sender.go
@@ -596,11 +596,6 @@ func (s *sender) sendOTLPMetrics(ctx context.Context, md pmetric.Metrics) error 
 	for i := 0; i < rms.Len(); i++ {
 		rm := rms.At(i)
 
-		if s.config.TranslateAttributes {
-			translateAttributes(rm.Resource().Attributes()).
-				CopyTo(rm.Resource().Attributes())
-		}
-
 		s.addSourceResourceAttributes(rm.Resource().Attributes())
 	}
 