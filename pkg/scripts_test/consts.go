c496b3a363bdd4156ea3ee6f9239c8b9b08c7f04
Dominik Rosiek <drosiek@sumologic.com>
2022-10-12 11:05:33 +0200

    feat(scripts/install.sh): require additional argument to remove data and configuration
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- pkg/scripts_test/consts.go
+++ pkg/scripts_test/consts.go
@@ -2,13 +2,15 @@ package sumologic_scripts_tests
 
 const (
 	binaryPath      string = "/usr/local/bin/otelcol-sumo"
-	fileStoragePath string = "/var/lib/sumologic/file_storage"
+	libPath         string = "/var/lib/otelcol-sumo"
+	fileStoragePath string = libPath + "/file_storage"
 	etcPath         string = "/etc/otelcol-sumo"
 	systemdPath     string = "/etc/systemd/system/otelcol-sumo.service"
 	scriptPath      string = "../../scripts/install.sh"
 	installToken    string = "token"
 	configPath      string = etcPath + "/sumologic.yaml"
-	userConfigPath  string = etcPath + "/conf.d/common.yaml"
+	confDPath       string = etcPath + "/conf.d"
+	userConfigPath  string = confDPath + "/common.yaml"
 
 	systemdDirectoryPath string = "/run/systemd/system"
 )
