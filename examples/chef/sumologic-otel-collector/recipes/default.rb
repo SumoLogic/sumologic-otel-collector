8fc1ebc479c7e54016763c24842df758da1b9692
Mikołaj Świątek <mswiatek@sumologic.com>
2022-10-20 18:07:29 +0200

    feat(examples): use install script in Chef recipe

--- examples/chef/sumologic-otel-collector/recipes/default.rb
+++ examples/chef/sumologic-otel-collector/recipes/default.rb
@@ -1,11 +1,4 @@
 sumologic_otel_collector 'sumologic-otel-collector' do
-  version '0.50.0-sumo-0'
-  src_config_path '/sumologic/examples/config_logging.yaml'
-  memory_high '200M'
-  memory_max '300M'
-  systemd_service true
-  os_family 'linux'
-  os_arch 'amd64'
-
-  action :default
+  install_token "dummy"
+  collector_tags ({"abc" => "def"})
 end
