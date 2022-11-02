798c6b2a1e562b998dd92a732d483bfbcb54f225
Jef Spaleta <jspaleta@gmail.com>
2022-11-02 13:42:50 -0800

    Update scripts/install.sh
    Co-authored-by: Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1009,7 +1009,6 @@ curl -fL "${SYSTEMD_CONFIG_URL}" --output "${TMP_SYSTEMD_CONFIG}" --progress-bar
 sed -i.bak -e "s%/etc/otelcol-sumo%'${CONFIG_DIRECTORY}'%" "${TMP_SYSTEMD_CONFIG}"
 sed -i.bak -e "s%/etc/otelcol-sumo/env%'${USER_ENV_DIRECTORY}'%" "${TMP_SYSTEMD_CONFIG}"
 
-
 # Remove glob for versions up to 0.57
 if (( $(echo "${VERSION_PREFIX} <= 0.57" | bc -l) )); then
     sed -i.bak -e "s% --config \"glob.*\"% --config ${COMMON_CONFIG_PATH}%" "${TMP_SYSTEMD_CONFIG}"
