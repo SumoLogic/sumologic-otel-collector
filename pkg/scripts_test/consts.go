63a7817d127cf94cafe9043a9405be9773c34e59
Mikołaj Świątek <mswiatek@sumologic.com>
2022-11-04 12:40:20 +0100

    test(scripts/install.sh): check config path permissions and ownership

--- pkg/scripts_test/consts.go
+++ pkg/scripts_test/consts.go
@@ -1,15 +1,16 @@
 package sumologic_scripts_tests
 
 const (
-	binaryPath      string = "/usr/local/bin/otelcol-sumo"
-	libPath         string = "/var/lib/otelcol-sumo"
-	fileStoragePath string = libPath + "/file_storage"
-	etcPath         string = "/etc/otelcol-sumo"
-	systemdPath     string = "/etc/systemd/system/otelcol-sumo.service"
-	scriptPath      string = "../../scripts/install.sh"
-	configPath      string = etcPath + "/sumologic.yaml"
-	confDPath       string = etcPath + "/conf.d"
-	userConfigPath  string = confDPath + "/common.yaml"
+	binaryPath            string = "/usr/local/bin/otelcol-sumo"
+	libPath               string = "/var/lib/otelcol-sumo"
+	fileStoragePath       string = libPath + "/file_storage"
+	etcPath               string = "/etc/otelcol-sumo"
+	systemdPath           string = "/etc/systemd/system/otelcol-sumo.service"
+	scriptPath            string = "../../scripts/install.sh"
+	configPath            string = etcPath + "/sumologic.yaml"
+	configPathPermissions uint32 = 0440
+	confDPath             string = etcPath + "/conf.d"
+	userConfigPath        string = confDPath + "/common.yaml"
 
 	systemdDirectoryPath string = "/run/systemd/system"
 
