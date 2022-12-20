69d20968a595452424f9227c27fe376445072bcc
Adam Boguszewski <aboguszewski@sumologic.com>
2022-12-20 15:13:38 +0100

    feat(sumologicschemaprocessor): add tests for nesting values

--- pkg/processor/sumologicschemaprocessor/nesting_processor_test.go
+++ pkg/processor/sumologicschemaprocessor/nesting_processor_test.go
@@ -17,13 +17,94 @@ package sumologicschemaprocessor
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
 func TestNestingAttributes(t *testing.T) {
-	_ = []struct {
+	testCases := []struct {
 		name     string
 		input    map[string]pcommon.Value
 		expected map[string]pcommon.Value
-	}{}
+	}{
+		{
+			name: "sample nesting",
+			input: map[string]pcommon.Value{
+				"kubernetes.container_name": pcommon.NewValueStr("xyz"),
+				"kubernetes.host.name":      pcommon.NewValueStr("the host"),
+				"kubernetes.host.address":   pcommon.NewValueStr("127.0.0.1"),
+				"kubernetes.namespace_name": pcommon.NewValueStr("sumologic"),
+				"another_attr":              pcommon.NewValueStr("42"),
+			},
+			expected: map[string]pcommon.Value{
+				"kubernetes": mapToPcommonValue(map[string]pcommon.Value{
+					"container_name": pcommon.NewValueStr("xyz"),
+					"namespace_name": pcommon.NewValueStr("sumologic"),
+					"host": mapToPcommonValue(map[string]pcommon.Value{
+						"name":    pcommon.NewValueStr("the host"),
+						"address": pcommon.NewValueStr("127.0.0.1"),
+					}),
+				}),
+				"another_attr": pcommon.NewValueStr("42"),
+			},
+		},
+		{
+			name: "single values",
+			input: map[string]pcommon.Value{
+				"a": mapToPcommonValue(map[string]pcommon.Value{
+					"b": mapToPcommonValue(map[string]pcommon.Value{
+						"c": pcommon.NewValueStr("d"),
+					}),
+				}),
+				"a.b.c": pcommon.NewValueStr("d"),
+				"d.g.e": pcommon.NewValueStr("l"),
+				"b.g.c": pcommon.NewValueStr("bonus"),
+			},
+			expected: map[string]pcommon.Value{
+				"a": mapToPcommonValue(map[string]pcommon.Value{
+					"b": mapToPcommonValue(map[string]pcommon.Value{
+						"c": pcommon.NewValueStr("d"),
+					}),
+				}),
+				"d": mapToPcommonValue(map[string]pcommon.Value{
+					"g": mapToPcommonValue(map[string]pcommon.Value{
+						"e": pcommon.NewValueStr("l"),
+					}),
+				}),
+				"b": mapToPcommonValue(map[string]pcommon.Value{
+					"g": mapToPcommonValue(map[string]pcommon.Value{
+						"c": pcommon.NewValueStr("bonus"),
+					}),
+				}),
+			},
+		},
+		{
+			name: "overwrite map with simple value",
+			input: map[string]pcommon.Value{
+				"sumo.logic": pcommon.NewValueBool(true),
+				"sumo":       pcommon.NewValueBool(false),
+			},
+			expected: map[string]pcommon.Value{
+				"sumo": mapToPcommonValue(map[string]pcommon.Value{
+					"":      pcommon.NewValueBool(false),
+					"logic": pcommon.NewValueBool(true),
+				}),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			proc, err := newNestingProcessor(".", true)
+			require.NoError(t, err)
+
+			attrs := mapToPcommonMap(testCase.input)
+			err = proc.processAttributes(attrs)
+			require.NoError(t, err)
+
+			expected := mapToPcommonMap(testCase.expected)
+
+			require.Equal(t, expected.AsRaw(), attrs.AsRaw())
+		})
+	}
 }
