6e2546fd4f286bf425c48101c88edecf76552205
Mikołaj Świątek <mswiatek@sumologic.com>
2023-02-01 12:25:17 +0100

    fix(scripts/install.sh): config dir permissions
    Set permissions for config directory in /etc/ before putting config
    files there. /etc/ is world-readable by default, and credentials can be
    leaked this way.

--- pkg/scripts_test/consts.go
+++ pkg/scripts_test/consts.go
@@ -5,7 +5,7 @@ const (
 	libPath                   string = "/var/lib/otelcol-sumo"
 	fileStoragePath           string = libPath + "/file_storage"
 	etcPath                   string = "/etc/otelcol-sumo"
-	etcPathPermissions        uint32 = 0555
+	etcPathPermissions        uint32 = 0551
 	systemdPath               string = "/etc/systemd/system/otelcol-sumo.service"
 	scriptPath                string = "../../scripts/install.sh"
 	configPath                string = etcPath + "/sumologic.yaml"
