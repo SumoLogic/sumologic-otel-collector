9eca6457dcbff7687314821f28b19ac8b5d0babe
Raj Nishtala <113392743+rnishtala-sumo@users.noreply.github.com>
2023-08-23 19:34:14 -0400

    fix(chef): Change the readme path for the chef cookbook (#1229)

--- examples/chef/sumologic-otel-collector/metadata.rb
+++ examples/chef/sumologic-otel-collector/metadata.rb
@@ -3,6 +3,6 @@ maintainer 'Sumo Logic'
 maintainer_email 'collection@sumologic.com'
 license 'Apache-2.0'
 description 'Installs sumologic-otel-collector'
-long_description IO.read(File.join(File.dirname(__FILE__), '../README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.0'
 chef_version '>= 11' if respond_to?(:chef_version)
