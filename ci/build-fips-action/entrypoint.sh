5e504436f2b59a5e65d32ee97aee9e83e16a1d4d
Justin Kolberg <amd.prophet@gmail.com>
2023-09-25 11:30:00 -0700

    chore(ci): add build-fips make target
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- ci/build-fips-action/entrypoint.sh
+++ ci/build-fips-action/entrypoint.sh
@@ -2,13 +2,6 @@
 
 git config --global --add safe.directory /github/workspace
 
-# Install Go
-url="https://go.dev/dl/go${GO_VERSION}.linux-${TARGETARCH}.tar.gz"
-echo "Downloading ${url}"
-curl -Lo go.tar.gz "$url"
-tar -zxvf go.tar.gz -C /usr/local
-export PATH="/usr/local/go/bin:${PATH}"
-
 # Install builder
 cd otelcolbuilder || exit 1
 mkdir "${HOME}/bin"
