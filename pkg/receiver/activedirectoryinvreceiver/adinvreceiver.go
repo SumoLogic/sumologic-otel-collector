0bb57ef029a787a1eb0e849922d487093e22f6f9
Raj Nishtala <rnishtala@sumologic.com>
2023-10-05 15:10:28 -0400

    Adding initial tests and readme

--- pkg/receiver/activedirectoryinvreceiver/adinvreceiver.go
+++ pkg/receiver/activedirectoryinvreceiver/adinvreceiver.go
@@ -1,3 +1,17 @@
+// Copyright 2021, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package activedirectoryinvreceiver
 
 import (
@@ -35,7 +49,7 @@ func newLogsReceiver(cfg *ADConfig, logger *zap.Logger, consumer consumer.Logs) 
 }
 
 func (l *ADReceiver) Start(ctx context.Context, _ component.Host) error {
-	l.logger.Debug("starting to poll for Cloudwatch logs")
+	l.logger.Debug("starting to poll for active directory inventory records")
 	l.wg.Add(1)
 	go l.startPolling(ctx)
 	return nil
@@ -50,7 +64,7 @@ func (l *ADReceiver) Shutdown(_ context.Context) error {
 
 func (l *ADReceiver) startPolling(ctx context.Context) {
 	defer l.wg.Done()
-	t := time.NewTicker(l.config.PollInterval)
+	t := time.NewTicker(l.config.PollInterval * time.Second)
 	for {
 		select {
 		case <-ctx.Done():
---
+++ pkg/receiver/activedirectoryinvreceiver/adinvreceiver_test.go
@@ -0,0 +1,39 @@
+// Copyright 2023, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package activedirectoryinvreceiver
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.uber.org/zap"
+)
+
+func TestStart(t *testing.T) {
+	cfg := CreateDefaultConfig().(*ADConfig)
+	cfg.CN = "test user"
+
+	sink := &consumertest.LogsSink{}
+	logsRcvr := newLogsReceiver(cfg, zap.NewNop(), sink)
+
+	err := logsRcvr.Start(context.Background(), componenttest.NewNopHost())
+	require.NoError(t, err)
+
+	err = logsRcvr.Shutdown(context.Background())
+	require.NoError(t, err)
+}
