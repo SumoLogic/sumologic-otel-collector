d4dc86c3aa15de17cd330a30ea4662f2782020d7
Raj Nishtala <rnishtala@sumologic.com>
2023-10-05 11:43:01 -0400

    Add support for polling interval

--- pkg/receiver/activedirectoryinvreceiver/config.go
+++ pkg/receiver/activedirectoryinvreceiver/config.go
@@ -1,11 +1,14 @@
 package activedirectoryinvreceiver
 
+import "time"
+
 // ADConfig defines configuration for Active Directory Inventory receiver.
 
 type ADConfig struct {
-	CN       string `mapstructure:"cn"`
-	OU       string `mapstructure:"ou"`
-	Password string `mapstructure:"password"`
-	DC       string `mapstructure:"domain"`
-	Host     string `mapstructure:"host"`
+	CN           string        `mapstructure:"cn"`
+	OU           string        `mapstructure:"ou"`
+	Password     string        `mapstructure:"password"`
+	DC           string        `mapstructure:"domain"`
+	Host         string        `mapstructure:"host"`
+	PollInterval time.Duration `mapstructure:"poll_interval"`
 }
