6b969e36a977a5b122add478d8a6f80cb7168286
Christian Kruse <ctkruse99@gmail.com>
2023-11-06 12:23:17 -0800

    Build linux_arm64 and amd64 fips static binaries
    Signed-off-by: Christian Kruse <ctkruse99@gmail.com>

--- ci/build-fips-action/entrypoint.sh
+++ ci/build-fips-action/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+PLATFORM=$1
+
 git config --global --add safe.directory /github/workspace
 
 # Install builder
@@ -8,5 +10,21 @@ mkdir "${HOME}/bin"
 export PATH="${HOME}/bin:${PATH}"
 make install-builder
 
+# Detect if cross CC needs set
+case $PLATFORM in
+        'linux_amd64')
+                ARCH='x86_64';
+                ;;
+        'linux_arm64')
+                ARCH='aarch64';
+                ;;
+esac
+
+CC=''
+if [ "$(uname -m)" != "$ARCH" ]; then
+        CC="/opt/cross/${ARCH}-linux-musl/bin/${ARCH}-linux-musl-gcc"
+fi
+
+
 # Build otelcol-sumo
-make otelcol-sumo-linux_amd64 FIPS_SUFFIX="-fips" CGO_ENABLED="1"
+make otelcol-sumo-"${PLATFORM}" FIPS_SUFFIX="-fips" CGO_ENABLED="1" CC="$CC"
