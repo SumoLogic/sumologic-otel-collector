6363cde3777515dbf1d3b325985cc6609694c7a8
Sean Porter <portertech@gmail.com>
2023-11-29 16:11:11 -0800

    updated write_installation_token for install_token -> installation_token
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1163,15 +1163,15 @@ function write_installation_token() {
     local ext_indentation
     readonly ext_indentation="${3}"
 
-    # ToDo: ensure we override only sumologic `install_token`
-    if grep "install_token" "${file}" > /dev/null; then
+    # ToDo: ensure we override only sumologic `installation_token`
+    if grep "installation_token" "${file}" > /dev/null; then
         # Do not expose token in sed command as it can be saw on processes list
-        echo "s/install_token:.*$/install_token: $(escape_sed "${token}")/" | sed -i.bak -f - "${file}"
+        echo "s/installation_token:.*$/installation_token: $(escape_sed "${token}")/" | sed -i.bak -f - "${file}"
     else
         # write installation token on the top of sumologic: extension
         # Do not expose token in sed command as it can be saw on processes list
         echo "s/sumologic:/sumologic:\\
-\\${ext_indentation}install_token: $(escape_sed "${token}")/" | sed -i.bak -f - "${file}"
+\\${ext_indentation}installation_token: $(escape_sed "${token}")/" | sed -i.bak -f - "${file}"
     fi
 }
 
