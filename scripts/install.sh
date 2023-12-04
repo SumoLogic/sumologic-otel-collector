1966434ea5b816546ef639e68cf10098621a39c8
Sean Porter <portertech@gmail.com>
2023-12-04 09:26:27 -0800

    begin with deprecated install_token to support migrating to installation_token
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -989,16 +989,16 @@ function get_user_config() {
     fi
 
     # extract installation_token and strip quotes
-    # fallback to deprecated install_token
-    grep -m 1 installation_token "${file}" \
-        | sed 's/.*installation_token:[[:blank:]]*//' \
+    # begin with deprecated install_token to support migrating to installation_token
+    grep -m 1 install_token "${file}" \
+        | sed 's/.*install_token:[[:blank:]]*//' \
         | sed 's/[[:blank:]]*$//' \
         | sed 's/^"//' \
         | sed "s/^'//" \
         | sed 's/"$//' \
         | sed "s/'\$//" \
-    || grep -m 1 install_token "${file}" \
-        | sed 's/.*install_token:[[:blank:]]*//' \
+    || grep -m 1 installation_token "${file}" \
+        | sed 's/.*installation_token:[[:blank:]]*//' \
         | sed 's/[[:blank:]]*$//' \
         | sed 's/^"//' \
         | sed "s/^'//" \
