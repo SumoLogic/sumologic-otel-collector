8d723b4a3f7fed1e8c041ab840fbff3b1b42ed04
Sean Porter <portertech@gmail.com>
2023-12-13 10:16:25 -0800

    Remove opamp remote config dir readable validation (#1385)
    * remove opamp remote config dir readable validation
    
    Signed-off-by: Sean Porter <portertech@gmail.com>
    
    * changelog entry
    
    Signed-off-by: Sean Porter <portertech@gmail.com>
    
    ---------
    
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- pkg/extension/opampextension/config.go
+++ pkg/extension/opampextension/config.go
@@ -16,8 +16,6 @@ package opampextension
 
 import (
 	"errors"
-	"fmt"
-	"os"
 
 	"github.com/oklog/ulid/v2"
 	"go.opentelemetry.io/collector/component"
@@ -61,11 +59,6 @@ func (cfg *Config) Validate() error {
 
 	if cfg.RemoteConfigurationDirectory == "" {
 		return errors.New("opamp remote_configuration_directory must be provided")
-	}
-
-	d := cfg.RemoteConfigurationDirectory
-	if _, err := os.Stat(d); err != nil {
-		return fmt.Errorf("opamp remote_configuration_directory %s must be readable: %v", d, err)
 	}
 
 	return nil
