29eaf26cda6c71697da4e112bb7220e277db02f2
Adam Boguszewski <aboguszewski@sumologic.com>
2024-02-19 13:05:34 +0100

    fix build tag name

--- ci/build-push-multiplatform.sh
+++ ci/build-push-multiplatform.sh
@@ -82,7 +82,7 @@ function build_push() {
     esac
 
     local TAG
-    readonly TAG="${REPO_URL}:${BUILD_TAG}${LATEST_TAG_FIPS_SUFFIX}-${BUILD_ARCH}"
+    readonly TAG="${REPO_URL}:${BUILD_TAG}${BUILD_TYPE_SUFFIX}-${BUILD_ARCH}"
     local LATEST_TAG
     readonly LATEST_TAG="${REPO_URL}:latest${BUILD_TYPE_SUFFIX}-${BUILD_ARCH}"
 
