04f6cb0abc3b871f2fe89f5d8218fbc689bb04a9
Jaganathan P <156800415+jagan2221@users.noreply.github.com>
2024-06-11 11:03:10 +0530

    Update provider.go

--- pkg/configprovider/opampprovider/provider.go
+++ pkg/configprovider/opampprovider/provider.go
@@ -69,6 +69,41 @@ func NewFactory() confmap.ProviderFactory {
 	return confmap.NewProviderFactory(NewWithSettings)
 }
 
+// Remove specific keys from srcMap if same keys present in mergeMap under same path
+func removeMatchingKeysFromSrcMap(srcMap map[string]interface{}, mergeMap map[string]interface{}) map[string]interface{} {
+	srcMapVal := srcMap
+	mergeMapVal := mergeMap
+	for key, mergeValue := range mergeMapVal {
+		srcValue, exists := srcMapVal[key]
+
+		//if key not exists in merge map, skip the path
+		if !exists {
+			continue
+		}
+
+		if key == "collector_fields" {
+			delete(srcMapVal, key)
+		}
+
+		mergeNestedMap, isMergeMap := mergeValue.(map[string]interface{})
+		srcNestedMap, isSrcMap := srcValue.(map[string]interface{})
+
+		if isMergeMap && isSrcMap {
+			// Recursively handle nested maps
+			removeMatchingKeysFromSrcMap(srcNestedMap, mergeNestedMap)
+		}
+	}
+	return srcMap
+}
+
+// Checks and prepares scrConf for replace behavior instead of map merge
+// Hack for confmap.Conf.Merge method to replace specific fields instead of merging them
+// Merge method merges field values from source and mergeConf, so by removing existing values from
+// source map, we can achieve replace behavior
+func PrepareForReplaceBehavior(srcConf *confmap.Conf, mergeConf *confmap.Conf) {
+	*srcConf = *confmap.NewFromStringMap(removeMatchingKeysFromSrcMap(srcConf.ToStringMap(), mergeConf.ToStringMap()))
+}
+
 func (p *Provider) Retrieve(ctx context.Context, configPath string, fn confmap.WatcherFunc) (*confmap.Retrieved, error) {
 	var cfg ConfigFragment
 	configPath = strings.TrimPrefix(configPath, "opamp:")
@@ -100,6 +135,7 @@ func (p *Provider) Retrieve(ctx context.Context, configPath string, fn confmap.W
 	if err != nil {
 		return nil, err
 	}
+	PrepareForReplaceBehavior(retConf, addlConf)
 	// merge the file config in
 	if err := conf.Merge(addlConf); err != nil {
 		return nil, err
