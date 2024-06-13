3ffa98372ac95d960fcfe8efbdecd59365333bb1
Jaganathan P <jaganathan.p@sumologic.com>
2024-06-13 14:32:12 +0530

    refactor

--- pkg/configprovider/opampprovider/provider.go
+++ pkg/configprovider/opampprovider/provider.go
@@ -17,14 +17,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 	"github.com/SumoLogic/sumologic-otel-collector/pkg/configprovider/globprovider"
 	"github.com/SumoLogic/sumologic-otel-collector/pkg/configprovider/providerutil"
 	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
 	"gopkg.in/yaml.v2"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const (
@@ -67,41 +67,6 @@ func NewWithSettings(settings confmap.ProviderSettings) confmap.Provider {
 
 func NewFactory() confmap.ProviderFactory {
 	return confmap.NewProviderFactory(NewWithSettings)
-}
-
-// Remove specific keys from srcMap if same keys present in mergeMap under same path
-func removeMatchingKeysFromSrcMap(srcMap map[string]interface{}, mergeMap map[string]interface{}) map[string]interface{} {
-	srcMapVal := srcMap
-	mergeMapVal := mergeMap
-	for key, mergeValue := range mergeMapVal {
-		srcValue, exists := srcMapVal[key]
-
-		//if key not exists in merge map, skip the path
-		if !exists {
-			continue
-		}
-
-		if key == "collector_fields" {
-			delete(srcMapVal, key)
-		}
-
-		mergeNestedMap, isMergeMap := mergeValue.(map[string]interface{})
-		srcNestedMap, isSrcMap := srcValue.(map[string]interface{})
-
-		if isMergeMap && isSrcMap {
-			// Recursively handle nested maps
-			removeMatchingKeysFromSrcMap(srcNestedMap, mergeNestedMap)
-		}
-	}
-	return srcMap
-}
-
-// Checks and prepares scrConf for replace behavior instead of map merge
-// Hack for confmap.Conf.Merge method to replace specific fields instead of merging them
-// Merge method merges field values from source and mergeConf, so by removing existing values from
-// source map, we can achieve replace behavior
-func PrepareForReplaceBehavior(srcConf *confmap.Conf, mergeConf *confmap.Conf) {
-	*srcConf = *confmap.NewFromStringMap(removeMatchingKeysFromSrcMap(srcConf.ToStringMap(), mergeConf.ToStringMap()))
 }
 
 func (p *Provider) Retrieve(ctx context.Context, configPath string, fn confmap.WatcherFunc) (*confmap.Retrieved, error) {
