18b3a9228631cc9d1f566f9e3866ab4591804818
Harsh Shah <harsh.shah@sumologic.com>
2024-08-07 14:02:40 -0500

    reverting few testcases which are not required to be parametrized

--- pkg/extension/opampextension/opamp_agent_test.go
+++ pkg/extension/opampextension/opamp_agent_test.go
@@ -117,229 +117,195 @@ func TestApplyRemoteConfig(t *testing.T) {
 	}
 }
 
-func TestOpampAgenAuthNil(t *testing.T) {
-	testCases := []struct {
-		name     string
-		validate func(*opampAgent, *testing.T)
-	}{
-		{
-			name: "Start",
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.NoError(t, o.Start(context.Background(), componenttest.NewNopHost()))
-			},
-		},
-		{
-			name: "Reload",
-			validate: func(o *opampAgent, t *testing.T) {
-				ctx := context.Background()
-				assert.NoError(t, o.Start(ctx, componenttest.NewNopHost()))
-				assert.NoError(t, o.Reload(ctx))
-			},
-		},
-	}
+func TestGetAgentCapabilities(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			d, err := os.MkdirTemp("", "opamp.d")
-			assert.NoError(t, err)
-			defer os.RemoveAll(d)
-			cfg, set := setupWithRemoteConfig(t, d)
-			cfg.ClientConfig.Auth = nil
-			o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
-			assert.NoError(t, err)
-			tc.validate(o, t)
-		})
-	}
+	assert.Equal(t, o.getAgentCapabilities(), protobufs.AgentCapabilities(4102))
+
+	cfg.AcceptsRemoteConfiguration = false
+	assert.Equal(t, o.getAgentCapabilities(), protobufs.AgentCapabilities(4))
 }
-func TestDefaultOpampAgent(t *testing.T) {
-	testCases := []struct {
-		name     string
-		validate func(*opampAgent, *testing.T)
+
+func TestCreateAgentDescription(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	assert.Nil(t, o.agentDescription)
+	assert.NoError(t, o.createAgentDescription())
+	assert.NotNil(t, o.agentDescription)
+}
+
+func TestLoadEffectiveConfig(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	assert.Equal(t, len(o.effectiveConfig), 0)
+
+	assert.NoError(t, o.loadEffectiveConfig("testdata"))
+	assert.NotEqual(t, len(o.effectiveConfig), 0)
+}
+
+func TestSaveEffectiveConfig(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	d, err := os.MkdirTemp("", "opamp.d")
+	assert.NoError(t, err)
+	defer os.RemoveAll(d)
+
+	assert.NoError(t, o.saveEffectiveConfig(d))
+}
+
+func TestUpdateAgentIdentity(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	olduid := o.instanceId
+	assert.NotEmpty(t, olduid.String())
+
+	uid := ulid.Make()
+	assert.NotEqual(t, uid, olduid)
+
+	o.updateAgentIdentity(uid)
+	assert.Equal(t, o.instanceId, uid)
+}
+
+func TestComposeEffectiveConfig(t *testing.T) {
+	cfg, set := defaultSetup()
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	ec := o.composeEffectiveConfig()
+	assert.NotNil(t, ec)
+}
+
+func TestShutdown(t *testing.T) {
+	cfg, set := defaultSetup()
+	cfg.ClientConfig.Auth = nil
+
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	// Shutdown with no OpAMP client
+	assert.NoError(t, o.Shutdown(context.Background()))
+}
+
+// To be removed when the OpAMP backend correctly advertises its URL.
+// This test is badly written, it reaches into unexported fields to test
+// their contents, but given that it will be removed in a few months, that
+// shouldn't matter too much from a big picture perspective.
+func TestHackSetEndpoint(t *testing.T) {
+	tests := []struct {
+		name         string
+		url          string
+		wantEndpoint string
 	}{
 		{
-			name: "GetAgentCapabilities",
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.Equal(t, o.getAgentCapabilities(), protobufs.AgentCapabilities(4102))
-
-				o.cfg.AcceptsRemoteConfiguration = false
-				assert.Equal(t, o.getAgentCapabilities(), protobufs.AgentCapabilities(4))
-			},
+			name: "empty url defaults to config endpoint",
+			wantEndpoint: "wss://example.com",
 		},
 		{
-			name: "CreateAgentDescription",
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.Nil(t, o.agentDescription)
-				assert.NoError(t, o.createAgentDescription())
-				assert.NotNil(t, o.agentDescription)
-			},
+			name:         "url variant a",
+			url:          "https://sumo-open-events.example.com",
+			wantEndpoint: "wss://sumo-opamp-events.example.com/v1/opamp",
 		},
 		{
-			name: "LoadEffectiveConfig",
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.Equal(t, len(o.effectiveConfig), 0)
-
-				assert.NoError(t, o.loadEffectiveConfig("testdata"))
-				assert.NotEqual(t, len(o.effectiveConfig), 0)
-			},
+			name:         "url variant b",
+			url:          "https://sumo-open-collectors.example.com",
+			wantEndpoint: "wss://sumo-opamp-collectors.example.com/v1/opamp",
 		},
 		{
-			name: "SaveEffectiveConfig",
-			validate: func(o *opampAgent, t *testing.T) {
-				d, err := os.MkdirTemp("", "opamp.d")
-				assert.NoError(t, err)
-				defer os.RemoveAll(d)
-
-				assert.NoError(t, o.saveEffectiveConfig(d))
-			},
+			name:         "url variant c",
+			url:          "https://example.com",
+			wantEndpoint: "wss://example.com/v1/opamp",
 		},
 		{
-			name: "UpdateAgentIdentity",
-			validate: func(o *opampAgent, t *testing.T) {
-				olduid := o.instanceId
-				assert.NotEmpty(t, olduid.String())
-
-				uid := ulid.Make()
-				assert.NotEqual(t, uid, olduid)
-
-				o.updateAgentIdentity(uid)
-				assert.Equal(t, o.instanceId, uid)
-			},
+			name:         "dev sumologic url",
+			url:          "https://long-open-events.sumologic.net/api/v1",
+			wantEndpoint: "wss://long-opamp-events.sumologic.net/v1/opamp",
 		},
 		{
-			name: "ComposeEffectiveConfig",
-			validate: func(o *opampAgent, t *testing.T) {
-				ec := o.composeEffectiveConfig()
-				assert.NotNil(t, ec)
-			},
-		},
-		{
-			name: "Shutdown",
-			validate: func(o *opampAgent, t *testing.T) {
-				cfg := createDefaultConfig().(*Config)
-				cfg.ClientConfig.Auth = nil
+			name:         "prod sumologic url",
+			url:          "https://open-collectors.sumologic.com/api/v1",
+			wantEndpoint: "wss://opamp-collectors.sumologic.com/v1/opamp",
+		}, {
 
-				// Shutdown with no OpAMP client
-				assert.NoError(t, o.Shutdown(context.Background()))
-			},
-		},
-		{
-			name: "HackSetEndpoint",
-			validate: func(o *opampAgent, t *testing.T) {
-				tests := []struct {
-					name         string
-					url          string
-					wantEndpoint string
-				}{
-					{
-						name:         "empty url defaults to config endpoint",
-						wantEndpoint: "wss://example.com",
-					},
-					{
-						name:         "url variant a",
-						url:          "https://sumo-open-events.example.com",
-						wantEndpoint: "wss://sumo-opamp-events.example.com/v1/opamp",
-					},
-					{
-						name:         "url variant b",
-						url:          "https://sumo-open-collectors.example.com",
-						wantEndpoint: "wss://sumo-opamp-collectors.example.com/v1/opamp",
-					},
-					{
-						name:         "url variant c",
-						url:          "https://example.com",
-						wantEndpoint: "wss://example.com/v1/opamp",
-					},
-					{
-						name:         "dev sumologic url",
-						url:          "https://long-open-events.sumologic.net/api/v1",
-						wantEndpoint: "wss://long-opamp-events.sumologic.net/v1/opamp",
-					},
-					{
-						name:         "prod sumologic url",
-						url:          "https://open-collectors.sumologic.com/api/v1",
-						wantEndpoint: "wss://opamp-collectors.sumologic.com/v1/opamp",
-					},
-					{
-						name:         "prod sumologic url with region",
-						url:          "https://open-collectors.au.sumologic.com/api/v1/",
-						wantEndpoint: "wss://opamp-collectors.au.sumologic.com/v1/opamp",
-					},
-				}
-
-				for _, test := range tests {
-					t.Run(test.name, func(t *testing.T) {
-						agent := &opampAgent{cfg: &Config{
-							ClientConfig: confighttp.ClientConfig{
-								Endpoint: "wss://example.com",
-							},
-						}}
-						if err := agent.setEndpoint(test.url); err != nil {
-							// can only happen with an invalid URL, which is quite hard
-							// to even come up with for Go's URL package
-							t.Fatal(err)
-						}
-						if got, want := agent.endpoint, test.wantEndpoint; got != want {
-							t.Errorf("didn't get expected endpoint: got %q, want %q", got, want)
-						}
-					})
-				}
-			},
+			name:         "prod sumologic url with region",
+			url:          "https://open-collectors.au.sumologic.com/api/v1/",
+			wantEndpoint: "wss://opamp-collectors.au.sumologic.com/v1/opamp",
 		},
 	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg, set := defaultSetup()
-			o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
-			assert.NoError(t, err)
-			tc.validate(o, t)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			agent := &opampAgent{cfg: &Config{
+				ClientConfig: confighttp.ClientConfig{
+					Endpoint: "wss://example.com",
+				},
+			}}
+			if err := agent.setEndpoint(test.url); err != nil {
+				// can only happen with an invalid URL, which is quite hard
+				// to even come up with for Go's URL package
+				t.Fatal(err)
+			}
+			if got, want := agent.endpoint, test.wantEndpoint; got != want {
+				t.Errorf("didn't get expected endpoint: got %q, want %q", got, want)
+			}
 		})
 	}
 }
 
-func TestBuildInfoOpampAgent(t *testing.T) {
-	testCases := []struct {
-		name     string
-		setup    func() (*Config, extension.Settings)
-		validate func(*opampAgent, *testing.T)
-	}{
-		{
-			name: "NewOpampAgent",
-			setup: func() (*Config, extension.Settings) {
-				cfg, set := setupWithBuildInfo("test version", "otelcoltest")
-				return cfg, set
-			},
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.Equal(t, "otelcoltest", o.agentType)
-				assert.Equal(t, "test version", o.agentVersion)
-				assert.NotEmpty(t, o.instanceId.String())
-				assert.Empty(t, o.effectiveConfig)
-				assert.Nil(t, o.agentDescription)
-			},
-		},
-		{
-			name: "NewOpampAgentAttributes",
-			setup: func() (*Config, extension.Settings) {
-				cfg, set := setupWithBuildInfo("test version", "otelcoltest")
-				set.Resource.Attributes().PutStr(semconv.AttributeServiceName, "otelcol-sumo")
-				set.Resource.Attributes().PutStr(semconv.AttributeServiceVersion, "sumo.0")
-				set.Resource.Attributes().PutStr(semconv.AttributeServiceInstanceID, "f8999bc1-4c9b-4619-9bae-7f009d2411ec")
-				return cfg, set
-			},
-			validate: func(o *opampAgent, t *testing.T) {
-				assert.Equal(t, "otelcol-sumo", o.agentType)
-				assert.Equal(t, "sumo.0", o.agentVersion)
-				assert.Equal(t, "7RK6DW2K4V8RCSQBKZ02EJ84FC", o.instanceId.String())
-			},
-		},
-	}
+func TestNewOpampAgent(t *testing.T) {
+	cfg, set := setupWithBuildInfo("test version", "otelcoltest")
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+	assert.Equal(t, "otelcoltest", o.agentType)
+	assert.Equal(t, "test version", o.agentVersion)
+	assert.NotEmpty(t, o.instanceId.String())
+	assert.Empty(t, o.effectiveConfig)
+	assert.Nil(t, o.agentDescription)
+}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg, set := tc.setup()
-			o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
-			assert.NoError(t, err)
-			tc.validate(o, t)
-		})
-	}
+func TestNewOpampAgentAttributes(t *testing.T) {
+	cfg, set := setupWithBuildInfo("test version", "otelcoltest")
+	set.Resource.Attributes().PutStr(semconv.AttributeServiceName, "otelcol-sumo")
+	set.Resource.Attributes().PutStr(semconv.AttributeServiceVersion, "sumo.0")
+	set.Resource.Attributes().PutStr(semconv.AttributeServiceInstanceID, "f8999bc1-4c9b-4619-9bae-7f009d2411ec")
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+	assert.Equal(t, "otelcol-sumo", o.agentType)
+	assert.Equal(t, "sumo.0", o.agentVersion)
+	assert.Equal(t, "7RK6DW2K4V8RCSQBKZ02EJ84FC", o.instanceId.String())
+}
+
+func TestStart(t *testing.T) {
+	d, err := os.MkdirTemp("", "opamp.d")
+	assert.NoError(t, err)
+	defer os.RemoveAll(d)
+	cfg, set := setupWithRemoteConfig(t, d)
+	cfg.ClientConfig.Auth = nil
+	cfg.RemoteConfigurationDirectory = d
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	assert.NoError(t, o.Start(context.Background(), componenttest.NewNopHost()))
+}
+
+func TestReload(t *testing.T) {
+	d, err := os.MkdirTemp("", "opamp.d")
+	assert.NoError(t, err)
+	defer os.RemoveAll(d)
+	cfg, set := setupWithRemoteConfig(t, d)
+	cfg.ClientConfig.Auth = nil
+	o, err := newOpampAgent(cfg, set.Logger, set.BuildInfo, set.Resource)
+	assert.NoError(t, err)
+
+	ctx := context.Background()
+	assert.NoError(t, o.Start(ctx, componenttest.NewNopHost()))
+	assert.NoError(t, o.Reload(ctx))
 }
