f10e533362373d9c14799f8971f748561282904a
Jaganathan P <156800415+jagan2221@users.noreply.github.com>
2024-08-26 19:40:17 +0530

    Update opamp_agent.go

--- pkg/extension/opampextension/opamp_agent.go
+++ pkg/extension/opampextension/opamp_agent.go
@@ -429,6 +429,7 @@ func (o *opampAgent) saveEffectiveConfig(dir string) error {
 
 	for k, v := range o.effectiveConfig {
 		p := filepath.Join(dir, k)
+        	o.logger.Info("Loading file to Validate...", p)
 
 		// OpenFile the same way os.Create does it, but with 0600 perms
 		f, err := os.OpenFile(p, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
