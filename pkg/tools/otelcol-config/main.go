a165a2ed1b8287328975ea24eac44c87fd124767
Justin Kolberg <amd.prophet@gmail.com>
2024-09-26 16:08:34 -0700

    otelcol-config: fix ownership, perms, and hostmetrics filename (#1676)
    * otelcol-config: use hostmetrics.yaml as hostmetrics filename
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>
    
    * otelcol-config: fix ownership & perms for configs
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>
    
    * otelcol-config: remove unused setSumlogicRemoteOwner
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>
    
    * otelcol-config: add setConfigOwner stub for Windows
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>
    
    ---------
    
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- pkg/tools/otelcol-config/main.go
+++ pkg/tools/otelcol-config/main.go
@@ -15,9 +15,8 @@ import (
 )
 
 const (
-	hostmetricsLinux  = "hostmetrics-linux.yaml"
-	hostmetricsDarwin = "hostmetrics-darwin.yaml"
-	ephemeralYAML     = "ephemeral.yaml"
+	hostmetricsYAML = "hostmetrics.yaml"
+	ephemeralYAML   = "ephemeral.yaml"
 )
 
 // errorCoder is here to give actions a way to set the exit status of the program
@@ -92,31 +91,26 @@ func getSumologicRemoteWriter(values *flagValues) func([]byte) (int, error) {
 	return func(doc []byte) (int, error) {
 		if doc == nil {
 			// Special case: when doc is nil, we delete the file. This tells
-			// the packaging that it should not use --remote-config for
+			// the service managers that it should not use --remote-config for
 			// otelcol-sumo.
 			return 0, os.Remove(docPath)
 		}
 
+		// os.WriteFile sets permissions before umask so we must call os.Chmod
+		// after the file is created
 		if err := os.WriteFile(docPath, doc, 0660); err != nil {
 			return 0, fmt.Errorf("error writing sumologic-remote.yaml: %s", err)
 		}
 
-		if err := setSumologicRemoteOwner(values); err != nil {
+		if err := os.Chmod(docPath, 0660); err != nil {
+			return 0, fmt.Errorf("error setting sumologic-remote.yaml permissions: %s", err)
+		}
+
+		if err := setConfigOwner(values, docPath); err != nil {
 			return len(doc), fmt.Errorf("error setting sumologic-remote.yaml owner: %s", err)
 		}
 
 		return len(doc), nil
-	}
-}
-
-func getHostMetricsFilename() string {
-	switch runtime.GOOS {
-	case "linux":
-		return hostmetricsLinux
-	case "darwin":
-		return hostmetricsDarwin
-	default:
-		panic("unsupported os: " + runtime.GOOS)
 	}
 }
 
@@ -129,12 +123,18 @@ func getLinker(values *flagValues, filename string) func() error {
 	availPath := filepath.Join(values.ConfigDir, ConfDotDAvailable, filename)
 	configPath := filepath.Join(values.ConfigDir, ConfDotD, filename)
 	return func() error {
-		if err := os.Symlink(availPath, configPath); isLinkError(err) {
-			// if the link already exists, hostmetrics are already enabled
-			return nil
-		} else {
+		if err := os.Symlink(availPath, configPath); err != nil {
+			if isLinkError(err) {
+				// if the link already exists, feature is already enabled
+				return nil
+			}
 			return err
 		}
+
+		if err := setConfigOwner(values, configPath); err != nil {
+			return fmt.Errorf("error setting %s owner: %s", configPath, err)
+		}
+		return nil
 	}
 }
 
@@ -154,13 +154,11 @@ func getUnlinker(values *flagValues, filename string) func() error {
 }
 
 func getHostMetricsLinker(values *flagValues) func() error {
-	filename := getHostMetricsFilename()
-	return getLinker(values, filename)
+	return getLinker(values, hostmetricsYAML)
 }
 
 func getHostMetricsUnlinker(values *flagValues) func() error {
-	filename := getHostMetricsFilename()
-	return getUnlinker(values, filename)
+	return getUnlinker(values, hostmetricsYAML)
 }
 
 func getEphemeralLinker(values *flagValues) func() error {
