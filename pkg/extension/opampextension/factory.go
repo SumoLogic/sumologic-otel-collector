8329390a0d750e1f34a5dcddffc643e43fc3fc1a
Jaganathan P <156800415+jagan2221@users.noreply.github.com>
2024-12-04 08:44:06 +0530

    Update factory.go

--- pkg/extension/opampextension/factory.go
+++ pkg/extension/opampextension/factory.go
@@ -36,6 +36,7 @@ func createDefaultConfig() component.Config {
 	return &Config{
 		ClientConfig:               CreateDefaultClientConfig(),
 		AcceptsRemoteConfiguration: true,
+		NewConfigMergeFlowEnabled: true,
 	}
 }
 
