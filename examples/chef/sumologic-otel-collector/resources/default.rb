ffca4528b5111e93d6121fa6ab81b8bdd43ebdbb
Himanshu Singh <himanshu.singh@sumologic.com>
2025-10-15 12:14:32 +0530

    adding window support through chef (#1851)
    * adding window support through chef
    
    * changelog added
    
    * formatted code
    
    ---------
    
    Co-authored-by: Gourav garg <ggarg2906@gmail.com>
    Co-authored-by: Pankaj Kumar <pankaj.kumar@sumologic.com>

--- examples/chef/sumologic-otel-collector/resources/default.rb
+++ examples/chef/sumologic-otel-collector/resources/default.rb
@@ -21,24 +21,36 @@ property :remotely_managed, [true, false] , default: false
 # Sumo Logic Opamp Api url
 property :opamp_api_url, String
 
+# Linux specific constants
 DOWNLOAD_TIMEOUT = 300
 BINARY_PATH = '/usr/local/bin/otelcol-sumo'
 BINARY_CONFIG = '/etc/otelcol-sumo/conf.d'
 INSTALL_SCRIPT_PATH = "/tmp/install.sh"
 INSTALL_SCRIPT_URL = "https://download-otel.sumologic.com/latest/download/install.sh"
 
-action :default do
-  run_action :get_install_script
-  install_command = get_install_script_command(new_resource)
-  execute INSTALL_SCRIPT_PATH do
-    command install_command
-    environment ({"SUMOLOGIC_INSTALLATION_TOKEN" => new_resource.installation_token})
+# Windows specific constants
+WINDOWS_CONFIG_DIR = 'C:\\Program Files\\Sumo Logic\\OpenTelemetry Collector\\conf.d'
+WINDOWS_INSTALL_SCRIPT_URL = 'https://download-otel.sumologic.com/latest/download/install.ps1'
+WINDOWS_SERVICE_NAME = 'OtelcolSumo'
 
-  end
-  run_action :prepare_config
-  if new_resource.systemd_service
-    execute 'restart Otelcol' do
-      command 'systemctl restart otelcol-sumo'
+action :default do
+  if platform_family?('windows')
+    run_action :install_windows_collector
+    run_action :prepare_windows_config
+    run_action :restart_windows_service
+  else
+    run_action :get_install_script
+    install_command = get_install_script_command(new_resource)
+    execute INSTALL_SCRIPT_PATH do
+      command install_command
+      environment ({"SUMOLOGIC_INSTALLATION_TOKEN" => new_resource.installation_token})
+
+    end
+    run_action :prepare_config
+    if new_resource.systemd_service
+      execute 'restart Otelcol' do
+        command 'systemctl restart otelcol-sumo'
+      end
     end
   end
 end
@@ -83,4 +95,64 @@ def get_install_script_command(resource)
      command_parts.push("--skip-systemd")
   end
   command_parts.join(" ")
+end
+
+action :install_windows_collector do
+  powershell_script 'Install Sumo Logic Otel Collector (Windows)' do
+    code build_windows_install_command(new_resource)
+    action :run
+  end
+end
+
+action :prepare_windows_config do
+  if property_is_set?(:src_config_path)
+    directory WINDOWS_CONFIG_DIR do
+      recursive true
+      rights :full_control, 'Administrators'
+      action :create
+    end
+
+    remote_directory WINDOWS_CONFIG_DIR do
+      source new_resource.src_config_path
+      rights :full_control, 'Administrators'
+      action :create
+    end
+  end
+end
+
+action :restart_windows_service do
+  powershell_script 'Restart Sumo Logic Otel Collector Service (Windows)' do
+    code "Restart-Service -Name #{WINDOWS_SERVICE_NAME}"
+    action :run
+    only_if "Get-Service -Name #{WINDOWS_SERVICE_NAME} -ErrorAction SilentlyContinue"
+  end
+end
+
+# Helper to build the PowerShell command to install the collector on Windows
+def build_windows_install_command(resource)
+  tag_lines = resource.collector_tags.map { |k, v| "  '#{k}' = '#{v}'" }
+  tag_string = "@{\n#{tag_lines.join("\n")}\n}"
+
+  install_token = resource.installation_token.gsub('"', '`"')
+  api_url = resource.api_url.to_s.gsub('"', '`"')
+  opamp_api_url = resource.opamp_api_url.to_s.gsub('"', '`"')
+  version = resource.version.to_s.gsub('"', '`"')
+
+  command = <<~POWERSHELL
+    Set-ExecutionPolicy RemoteSigned -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    $uri = "#{WINDOWS_INSTALL_SCRIPT_URL}";
+    $path = "$env:TEMP\\install.ps1";
+    (New-Object System.Net.WebClient).DownloadFile($uri, $path);
+    $tags = #{tag_string};
+    & $path -InstallationToken "#{install_token}" -Tags $tags
+  POWERSHELL
+
+  command.chomp!
+  command << " -Api \"#{api_url}\"" if property_is_set?(:api_url)
+  command << " -OpAmpApi \"#{opamp_api_url}\"" if property_is_set?(:opamp_api_url)
+  command << " -Version \"#{version}\"" if property_is_set?(:version)
+  command << " -RemotelyManaged \$true" if resource.remotely_managed
+
+  command.strip
 end
