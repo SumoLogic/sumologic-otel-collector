f9ca9a15ad51ddbc147c34f99f5bff8cbb997771
Shubham Gupta <shubham.gupta@sumologic.com>
2026-01-28 13:37:58 +0530

    feat: add set collector name action

--- pkg/tools/otelcol-config/collector_name.go
+++ pkg/tools/otelcol-config/collector_name.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/mikefarah/yq/v4/pkg/yqlib"
+	"gopkg.in/yaml.v3"
+)
+
 func SetCollectorNameAction(ctx *actionContext) error {
+	conf, err := ReadConfigDir(ctx.ConfigDir)
+	if err != nil {
+		return err
+	}
+
+	var writer func([]byte) (int, error)
+	var doc []byte
+
+	switch {
+	case conf.SumologicRemote != nil || ctx.Flags.EnableRemoteControl:
+		writer = ctx.WriteSumologicRemote
+		doc = conf.SumologicRemote
+	case ctx.Flags.Override:
+		writer = ctx.WriteConfDOverrides
+		doc = conf.ConfD[ConfDOverrides]
+	default:
+		writer = ctx.WriteConfD
+		doc = conf.ConfD[ConfDSettings]
+
+	}
+
+	encoder := yqlib.YamlFormat.EncoderFactory()
+	decoder := yqlib.YamlFormat.DecoderFactory()
+	eval := yqlib.NewStringEvaluator()
+
+	if len(doc) == 0 {
+		buf := new(bytes.Buffer)
+		enc := yaml.NewEncoder(buf)
+		enc.SetIndent(2)
+		settings := map[string]any{
+			"extensions": map[string]any{
+				"sumologic": map[string]any{
+					"collector_name": ctx.Flags.SetCollectorName,
+				},
+			},
+		}
+		if err := enc.Encode(settings); err != nil {
+			panic(err)
+		}
+		doc = buf.Bytes()
+	} else {
+		expression := fmt.Sprintf(".extensions.sumologic.collector_name = %q", ctx.Flags.SetCollectorName)
+		result, err := eval.EvaluateAll(expression, string(doc), encoder, decoder)
+		if err != nil {
+			return fmt.Errorf("couldn't set collector name: error evaluating yq expression: %s", err)
+		}
+		if len(result) > 0 {
+			doc = []byte(result)
+			_, err = writer(doc)
+			if err != nil {
+				return fmt.Errorf("couldn't write updated config: %s", err)
+			}
+		}
+	}
 	return nil
 }
