60c58ffb60389c9e0b99ce148445c27472ae21ba
Raunak Mandaokar <132334083+raunakmandaokar@users.noreply.github.com>
2026-05-06 11:53:24 +0530

    fix(opamp): remove expandconverter from OpAMP config provider to fix $1 pattern crash (#2076)
    * fix(opamp): remove expandconverter from OpAMP config provider to fix $1 pattern crash
    
    * chore: added changelog

--- otelcolbuilder/cmd/configprovider.go
+++ otelcolbuilder/cmd/configprovider.go
@@ -22,7 +22,6 @@ import (
 	"slices"
 
 	"go.opentelemetry.io/collector/confmap"
-	"go.opentelemetry.io/collector/confmap/converter/expandconverter"
 	"go.opentelemetry.io/collector/confmap/provider/envprovider"
 	"go.opentelemetry.io/collector/featuregate"
 	"go.opentelemetry.io/collector/otelcol"
@@ -92,8 +91,7 @@ func NewOpAmpConfigProviderSettings(location string) otelcol.ConfigProviderSetti
 	return otelcol.ConfigProviderSettings{
 		ResolverSettings: confmap.ResolverSettings{
 			URIs:               []string{location},
-			ProviderFactories:  []confmap.ProviderFactory{opampprovider.NewFactory(), envprovider.NewFactory()},
-			ConverterFactories: []confmap.ConverterFactory{expandconverter.NewFactory()},
+			ProviderFactories: []confmap.ProviderFactory{opampprovider.NewFactory(), envprovider.NewFactory()},
 		},
 	}
 }
